17905752fbde93fa350990f2d9aaa42aef9ff6b3
vialvarez0603 <vialvarez@itba.edu.ar>
2024-11-19 16:20:00 -0300

    numero de documento agregado

--- patient.py
+++ patient.py
@@ -1,11 +1,19 @@
 from fhir.resources.patient import Patient
 from fhir.resources.humanname import HumanName
 from fhir.resources.contactpoint import ContactPoint
+from fhir.resources.identifier import Identifier
 
 # Crear el recurso FHIR de paciente con parámetros opcionales
-def create_patient_resource(family_name=None, given_name=None, birth_date=None, gender=None, phone=None):
+def create_patient_resource(document_number=None, family_name=None, given_name=None, birth_date=None, gender=None, phone=None):
     patient = Patient()
-    
+
+    # Agregar el identificador (número de documento) si está disponible
+    if document_number:
+        identifier = Identifier()
+        identifier.system = "http://CamiyVicky.org"  # Puede ser un identificador de sistema específico
+        identifier.value = document_number
+        patient.identifier = [identifier]
+
     # Agregar el nombre del paciente si está disponible
     if family_name or given_name:
         name = HumanName()
